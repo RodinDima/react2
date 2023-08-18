194aff7930cbb3ca980c657e8f4b65836d02cce4
rodindima <rodindima1984@gmail.com>
2023-08-18 15:32:40 +0300

    6666

--- src/components/SectionBuisness/SectionBusiness.jsx
+++ src/components/SectionBuisness/SectionBusiness.jsx
@@ -1,26 +1,44 @@
-import TextTemplate from '../TextTemplate/TextTemplate';
-import './reset.scss';
-import './style.scss';
-import style from './style.module.scss'; // Додайте цей рядок
-
-
-
 import React from 'react';
 
+import './style.scss';
 
 const SectionBusiness = () => {
-  //const fullText = "What We Do For Your Business";
-  //const words = fullText.split(' ');
-  //const firstTwoWords = words.slice(0, 3).join(' ');
-  //const remainingText = words.slice(3).join(' ');
-
   return (
     <section className='business'>
       <div className="business__container">
         <div className="business__content">
-          <TextTemplate>
-            <span className="special-text">What We Do</span> <span className={style['pseudo-class-text']}>For Your Business</span>
-          </TextTemplate>
+          {/*<TextTemplate>
+            <span className={style['special-text']}>What We Do</span> <span className={style['special-text']}>For Your</span> <span className={style['special-text']}>Business</span> <span className={style['pseudo-class-text']}>Fake or Real</span>
+          </TextTemplate>*/}
+          <h3 className="title content__title"><span>What We Do </span><span className='title-after'>For Your Business</span></h3> 
+          <div className="slider buisness__slider">
+            <div className="slider__box">
+              <div className="slider__item">
+                <div className="slider__item-img"></div>
+                <div className="slider__item-title">Website Design</div>
+                <div className="slider__item-text">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+              </div>
+                <div className="slider__item">
+                  <div className="slider__item-img"></div>
+                  <div className="slider__item-title">Website Design</div>
+                  <div className="slider__item-text">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+              </div>
+                <div className="slider__item">
+                  <div className="slider__item-img"></div>
+                  <div className="slider__item-title">Website Design</div>
+                  <div className="slider__item-text">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+              </div>
+                <div className="slider__item">
+                  <div className="slider__item-img"></div>
+                  <div className="slider__item-title">Website Design</div>
+                  <div className="slider__item-text">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+                </div>
+              </div>
+          </div>
+        
+                
+        
+        
         </div>
       </div>
     </section>

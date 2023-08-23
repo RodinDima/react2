e22f4f8ccd55afde52b42ef926830236a87eabba
rodindima <rodindima1984@gmail.com>
2023-08-23 16:58:55 +0300

    1111

--- src/components/SectionMain/SectionMain.jsx
+++ src/components/SectionMain/SectionMain.jsx
@@ -9,29 +9,35 @@ const SectionMain = () => {
   return (
     <>
       <main className="main">
-        
         <div className="main__container">
           <div className="main__content">
-          
-              <h3 className="main__title title"><span><TextTemplate className="firstStroke" text="What we do" /> </span><span className='title-after'>to grow your <span className='lastWord'>Busines</span></span></h3> 
-              <div className="main__text-paragraph">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
-              <Button
-                buttonText="Get Started"
-                onClick={() => {
-                  console.log('Button clicked');
-                }}
-                className="main-button"/>
-            
-            
+            <h3 className="main__title title">
+              <span>
+                <TextTemplate className="firstStroke" text="We Help you" />{" "}
+              </span>
+              <span className="title-after">
+                to grow your <span className="lastWord">Busines</span>
+              </span>
+            </h3>
+            <div className="main__text-paragraph">
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.
             </div>
-              
-             <div className='main__bg'>         
-                {/*<img className="yellow" src={Yellow} alt="My SVG" />
-                <div className='dashedCircle'></div>*/}
-                <img className="main__img" src={MainBg} alt="My SVG" />
-              </div>
+            <Button
+              buttonText="Get Started"
+              onClick={() => {
+                console.log("Button clicked");
+              }}
+              className="main-button"
+            />
           </div>
-       
+
+          <div className="image-container">
+            <img className="main__img" src={MainBg} alt="My SVG" />
+            {/*<img className="yellow" src={Yellow} alt="My SVG" />*/}
+          </div>
+        </div>
       </main>
       {/*<div className='rightTopCorner'></div>*/}
     </>

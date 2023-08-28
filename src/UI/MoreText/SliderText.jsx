0e04796d2512a7936a37e3edf18103292e5868ba
akilianchuiko <akilianchuiko@gmail.com>
2023-08-28 22:25:30 +0300

    add varr css

--- src/UI/MoreText/SliderText.jsx
+++ src/UI/MoreText/SliderText.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './style.scss';
-const SliderText = ({ text, isOpen }) => {
+
+const SliderText = ({ text, isOpen, id }) => {
+
+    console.log('isOpen', isOpen)
+    console.log('id', id)
     return (
-        <div className={`s-none ${ isOpen ? 'actives' : ''}`}>{text}</div>
+        <div className={`s-none ${ isOpen === id ? 'actives' : ''}`}>{text}</div>
     );
 }
 export default SliderText;
